f4946bd5fb7afe1e78ea4a02202810119fd83407
g0en <franco.barroso@alumnos.frm.utn.edu.ar>
2024-05-24 11:44:18 -0300

    cambios

--- src/main/java/com/example/buensaboruno/domain/entities/DetallePedido.java
+++ src/main/java/com/example/buensaboruno/domain/entities/DetallePedido.java
@@ -3,7 +3,10 @@ package com.example.buensaboruno.domain.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.OnDelete;
 import org.hibernate.envers.Audited;
+import org.hibernate.annotations.OnDeleteAction;
+
 
 @Entity
 @AllArgsConstructor
@@ -17,7 +20,9 @@ public class DetallePedido extends Base{
     private Integer cantidad;
     private Double subTotal;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "articulo_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Articulo articulo;
 
 }
